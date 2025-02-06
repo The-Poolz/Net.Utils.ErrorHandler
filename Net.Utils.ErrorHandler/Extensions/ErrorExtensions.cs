398d12070166c9aeaa4800a63e427107750f4074
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-02-06 18:31:59 +0300

    - initial commit

---
+++ Net.Utils.ErrorHandler/Extensions/ErrorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentValidation;
+using System.Reflection;
+using FluentValidation.Results;
+using Net.Utils.ErrorHandler.Attributes;
+
+namespace Net.Utils.ErrorHandler.Extensions
+{
+    public static class ErrorExtensions
+    {
+        public static ValidationException ToException(this Enum error, object? customState = null, string propertyName = "")
+        {
+            var errorMessage = error.ToErrorMessage();
+            var failure = new ValidationFailure(propertyName, errorMessage)
+            {
+                ErrorCode = error.ToErrorCode(),
+                CustomState = customState
+            };
+            return new ValidationException(errorMessage, new []{ failure });
+        }
+
+        public static string ToErrorCode(this Enum error)
+        {
+            return error.ToString();
+        }
+
+        public static string ToErrorMessage(this Enum error)
+        {
+            return error
+                .GetType()
+                .GetField(error.ToString())!
+                .GetCustomAttribute<ErrorAttribute>()!
+                .Message;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Net.Utils.ErrorHandler
+
 A C# library for managing error handling through enums and custom attributes.
+
+## Installation
+
+.NET CLI
+```powershell
+dotnet add package Net.Utils.ErrorHandler
+```
+
+Package Manager
+```powershell
+Install-Package Net.Utils.ErrorHandler
+```
