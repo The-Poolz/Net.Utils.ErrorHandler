e6b87ddcb22a6db26cc90e71ff6e79db63d9b2a3
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-02-07 13:47:45 +0300

    - add tests for `ToException`

--- tests/Net.Utils.ErrorHandler.Tests/ErrorExtensionsTests.cs
+++ tests/Net.Utils.ErrorHandler.Tests/ErrorExtensionsTests.cs
@@ -14,6 +14,7 @@ public class ErrorExtensionsTests
         internal void ShouldReturnsStringRepresentationOfEnumMember(Error error, string expected)
         {
             var errorCode = error.ToErrorCode();
+
             errorCode.Should().Be(expected);
         }
     }
@@ -26,6 +27,7 @@ public class ErrorExtensionsTests
         internal void WhenAttributeImplemented_ShouldReturnsExpectedMessage(Error error, string expected)
         {
             var errorMessage = error.ToErrorMessage();
+
             errorMessage.Should().Be(expected);
         }
 
@@ -33,8 +35,43 @@ public class ErrorExtensionsTests
         internal void WhenAttributeNotImplemented_ShouldThrowArgumentException()
         {
             var testCode = () => Error.ENUM_MEMBER_WITHOUT_ERROR_ATTRIBUTE.ToErrorMessage();
+
             testCode.Should().Throw<ArgumentException>()
                 .WithMessage("Member 'ENUM_MEMBER_WITHOUT_ERROR_ATTRIBUTE' of 'Net.Utils.ErrorHandler.Tests.Error' enum must implement 'ErrorAttribute' attribute. (Parameter 'error')");
+        }
+    }
+
+    public class ToException
+    {
+        [Fact]
+        internal void WhenStateIsNull_ShouldReturnsExpectedError()
+        {
+            var errorCode = Error.NOT_FOUND.ToException();
+
+            errorCode.Message.Should().Be(ErrorMessages.NOT_FOUND);
+            errorCode.Errors.Should().ContainSingle(x => 
+                x.ErrorMessage == ErrorMessages.NOT_FOUND &&
+                x.ErrorCode == Error.NOT_FOUND.ToErrorCode() &&
+                x.CustomState == null
+            );
+        }
+
+        [Fact]
+        internal void WhenStateNotNull_ShouldReturnsExpectedError()
+        {
+            var data = new
+            {
+                MyCustomData = "Data will be here."
+            };
+            var errorCode = Error.NOT_FOUND.ToException(data);
+
+            errorCode.Message.Should().Be(ErrorMessages.NOT_FOUND);
+            errorCode.Errors.Should().HaveCount(1);
+            errorCode.Errors.Should().ContainSingle(x =>
+                x.ErrorMessage == ErrorMessages.NOT_FOUND &&
+                x.ErrorCode == Error.NOT_FOUND.ToErrorCode() &&
+                x.CustomState == data
+            );
         }
     }
 }
