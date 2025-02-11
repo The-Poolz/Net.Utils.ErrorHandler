e63156c1b34640e6b4ae7295e020f7714545f225
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-02-11 12:41:25 +0300

    New `WithError` extension method (#3)
    * - create `RuleBuilderOptionsExtensions`
    
    * - update version

---
+++ src/Net.Utils.ErrorHandler/Extensions/RuleBuilderOptionsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentValidation;
+
+namespace Net.Utils.ErrorHandler.Extensions
+{
+    public static class RuleBuilderOptionsExtensions
+    {
+        public static IRuleBuilderOptions<T, TProperty> WithError<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, Enum error, object? customState = null)
+        {
+            return rule
+                .WithState(_ => customState)
+                .WithErrorCode(error)
+                .WithErrorMessage(error);
+        }
+
+        public static IRuleBuilderOptions<T, TProperty> WithErrorMessage<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, Enum error)
+        {
+            return rule.WithMessage(error.ToErrorMessage());
+        }
+
+        public static IRuleBuilderOptions<T, TProperty> WithErrorCode<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, Enum error)
+        {
+            return rule.WithErrorCode(error.ToErrorCode());
+        }
+    }
+}
